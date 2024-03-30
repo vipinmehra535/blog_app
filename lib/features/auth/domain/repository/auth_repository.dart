9774fb8afd684d41504f8d7614c915eb131f5e59
vipinmehra535 <vipinsinghmehra535@gmail.com>
2024-03-30 12:53:34 +0530

    supabase setup Started

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,1 +1,15 @@
-abstract 
+import 'package:blog_app/core/error/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, String>> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
